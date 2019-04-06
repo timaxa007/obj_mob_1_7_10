8169c82cbbaadbc6af3b5a153cf99de2eb9daec7
timaxa007 <timaxa007@gmail.com>
2019-04-06 13:45:23 +0300

    update displayList

--- java/timaxa007/obj_mob/ModelMyMob.java
+++ java/timaxa007/obj_mob/ModelMyMob.java
@@ -4,28 +4,23 @@ import org.lwjgl.opengl.GL11;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.model.AdvancedModelLoader;
-import net.minecraftforge.client.model.IModelCustom;
 import timaxa007.test_armor_obj.ArmorCustomModel;
 
 @SideOnly(Side.CLIENT)
 public class ModelMyMob extends ArmorCustomModel {
 
-	public static final IModelCustom
-	model = AdvancedModelLoader.loadModel(new ResourceLocation(MyMod.MODID, "model/mob/ugly_biper.obj"));
-
 	public ModelMyMob() {
 		bipedHead.showModel =
-		bipedHeadwear.showModel =
-		bipedBody.showModel =
-		bipedRightArm.showModel =
-		bipedLeftArm.showModel =
-		bipedRightLeg.showModel =
-		bipedLeftLeg.showModel =
-		bipedEars.showModel =
-		bipedCloak.showModel =
-		false;
+				bipedHeadwear.showModel =
+				bipedBody.showModel =
+				bipedRightArm.showModel =
+				bipedLeftArm.showModel =
+				bipedRightLeg.showModel =
+				bipedLeftLeg.showModel =
+				bipedEars.showModel =
+				bipedCloak.showModel =
+				false;
+
 	}
 
 	@Override
@@ -41,37 +36,37 @@ public class ModelMyMob extends ArmorCustomModel {
 	@Override
 	public void partHead() {
 		GL11.glTranslatef(0F, -1.5F, 0F);
-		model.renderPart("head");
+		GL11.glCallList(ProxyClient.displayList[0]);
 	}
 
 	@Override
 	public void partBody() {
 		GL11.glTranslatef(0F, -1.5F, 0F);
-		model.renderPart("body");
+		GL11.glCallList(ProxyClient.displayList[1]);
 	}
 
 	@Override
 	public void partRightArm() {
 		GL11.glTranslatef(0.3125F, -1.375F, 0F);
-		model.renderPart("right_arm");
+		GL11.glCallList(ProxyClient.displayList[2]);
 	}
 
 	@Override
 	public void partLeftArm() {
 		GL11.glTranslatef(-0.3125F, -1.375F, 0F);
-		model.renderPart("left_arm");
+		GL11.glCallList(ProxyClient.displayList[3]);
 	}
 
 	@Override
 	public void partRightLeg() {
 		GL11.glTranslatef(0.125F, -0.75F, 0F);
-		model.renderPart("right_leg");
+		GL11.glCallList(ProxyClient.displayList[4]);
 	}
 
 	@Override
 	public void partLeftLeg() {
 		GL11.glTranslatef(-0.125F, -0.75F, 0F);
-		model.renderPart("left_leg");
+		GL11.glCallList(ProxyClient.displayList[5]);
 	}
 
 }
--- java/timaxa007/obj_mob/ProxyClient.java
+++ java/timaxa007/obj_mob/ProxyClient.java
@@ -1,11 +1,37 @@
 package timaxa007.obj_mob;
 
+import org.lwjgl.opengl.GL11;
+
 import cpw.mods.fml.client.registry.RenderingRegistry;
+import net.minecraft.client.renderer.GLAllocation;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.AdvancedModelLoader;
+import net.minecraftforge.client.model.IModelCustom;
 
 public class ProxyClient extends ProxyServer {
 
+	public static int[] displayList = new int[6];//for 6 parts model
+
 	@Override
 	public void preInit() {
+
+		final IModelCustom model = AdvancedModelLoader.loadModel(new ResourceLocation(MyMod.MODID, "model/mob/ugly_biper.obj"));
+		//ModelRenderer.compileDisplayList(float)
+		final String[] partsName = new String[] {
+				"head",			//1
+				"body",			//2
+				"right_arm",	//3
+				"left_arm",		//4
+				"right_leg",	//5
+				"left_leg"		//6
+		};
+		for (int i = 0; i < displayList.length; ++i ) {
+			displayList[i] = GLAllocation.generateDisplayLists(1);
+			GL11.glNewList(displayList[i], GL11.GL_COMPILE);
+			model.renderPart(partsName[i]);
+			GL11.glEndList();
+		}
+
 		RenderingRegistry.registerEntityRenderingHandler(EntityMyMob.class, new RenderEntityMyMob());
 	}
 
